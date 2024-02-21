a82b166913818f2251cf54ca9e305ddb178e400f
Christopher Stevens <chris@kawcreek.com>
2024-02-21 13:58:02 -0600

    Fixing Raycasting two scripts doesn't work.

--- Assets/Scripts/ItemInteraction.cs
+++ Assets/Scripts/ItemInteraction.cs
@@ -8,36 +8,43 @@ public class ItemInteraction : MonoBehaviour
     private string _selectTag = "Selection";
     private bool _isHighlighted = false;
     private Transform _selection;
-    public TMP_Text _nameDisplay;
+    public TMP_Text nameDisplay;
     public float distanceFromItem = 3f;
 
     private void Update()
     {
         if (_selection != null)
         {
+            nameDisplay.text = "";
             _isHighlighted = false;
+            //Renderer selectionRenderer = _selection.GetComponent<Renderer>();
+            //selectionRenderer.material.DisableKeyword("_EMISSION");
             _selection = null;
-            Renderer selectionRenderer = _selection.GetComponent<Renderer>();
-            selectionRenderer.material.DisableKeyword("_EMISSION");
         }
 
         var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
 
+        Debug.DrawRay(ray.origin, ray.direction * distanceFromItem, Color.red); 
+
         if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, distanceFromItem))
         {
+            Debug.Log("Test");
             var selection = hit.transform;
             if (selection.CompareTag(_selectTag))
             {
+                Debug.Log("Test2");
                 if (selection != _isHighlighted)
                 {
-                    selection.GetComponent<Renderer>().material.EnableKeyword("_EMISSION");
+                    Debug.Log("Test3");
                     _isHighlighted = true;
-                    _nameDisplay.text = selection.gameObject.name;
+                    //selection.GetComponent<Renderer>().material.EnableKeyword("_EMISSION");
+                    nameDisplay.text = selection.gameObject.name;
                 }
                 _selection = selection;
             }
         }
     }
 }
+   
 
