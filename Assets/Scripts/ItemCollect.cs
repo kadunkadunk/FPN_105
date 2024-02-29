31815dd17584d7903b6f299e1240affbfe8afba4
Christopher Stevens <chris@kawcreek.com>
2024-02-29 16:24:51 -0600

    Inventory extract into game working

--- Assets/Scripts/ItemCollect.cs
+++ Assets/Scripts/ItemCollect.cs
@@ -2,16 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 [RequireComponent(typeof(AudioSource))]
 public class ItemCollect : MonoBehaviour 
 {
     public Inventory inventory;
-    public GameObject objectToInstantiate;
     public AudioSource audioSource;
-
-
-
+    public GameObject player;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -19,28 +18,39 @@ public class ItemCollect : MonoBehaviour
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+     void Update()
     {
-        //Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        //RaycastHit hit;
-        //if (Physics.Raycast(ray, out hit, 3))
-        //{
-        //    Debug.Log(hit.transform.name);
-        //    if (inventory != null)
-        //    {
-        //        Debug.Log("Inventory is not null");
-        //        objectToInstantiate = inventory.inventoryItems.Find(x => x.name == "Hammer");
-        //        if (objectToInstantiate != null)
-        //        {
-        //            Debug.Log("Hammer found in inventory");
-        //            //audioSource.Play();
-        //            Instantiate(objectToInstantiate, hit.point, Quaternion.identity);
-        //        }
-        //    }
-          
-          
-        //}
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Mouse down event");
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
+            if (Physics.Raycast(ray, out hit, 3))
+            {
+                player = GameObject.FindWithTag("Player");
+                inventory = player.GetComponent<Inventory>();
+                GameObject paperclip = inventory.inventoryItems.Find(x => x.name == "Paperclip");
+                Debug.Log("Paperclip found in inventory: " + paperclip);
+
+                Debug.Log(hit.transform.name);
+                if (inventory != null)
+                {
+                    Debug.Log("Inventory is not null");
+                    if (paperclip != null)
+                    {
+                        Debug.Log("Paperclip created");
+                        //audioSource.Play();
+                        Instantiate(paperclip, hit.point, Quaternion.identity);
+                        paperclip.SetActive(true);
+                    }
+                }
+                else
+                {
+                    Debug.Log("Inventory is null");
+                }
+            }
+        }
     }
 
     private void OnTriggerEnter(Collider other)
